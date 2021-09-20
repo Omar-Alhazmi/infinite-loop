c9a683656d377b237a6548c050b11bc9cc507ee3
Ahmad <umb470@gmail.com>
2021-09-20 13:54:50 +0300

    display managment table with dummy data and styled table component

---
+++ src/FrontDesign/styledTabled.js
@@ -0,0 +1,110 @@
+import styled from 'styled-components'
+export const TableWrapper  = styled.div`
+    margin: 10px 70px 70px;
+    // box-shadow: 0px 35px 50px rgba( 0, 0, 0, 0.2 );
+    @media (max-width: 767px) {
+        &:before{
+        display: block;
+        text-align: right;
+        font-size: 11px;
+        color: white;
+        padding: 0 0 10px;
+    }
+}
+`
+export const TableContainer = styled.table`
+    border-radius: 5px;
+    font-size: 12px;
+    font-weight: normal;
+    border: none;
+    border-collapse: collapse;
+    width: 100%;
+    max-width: 100%;
+    white-space: nowrap;
+    background-color: white;
+    box-shadow: 0px 35px 50px rgba( 0, 0, 0, 0.2 );
+    @media (max-width: 767px) {
+    display: block;
+        width: 100%;
+ }
+`
+
+export const TableHedContainer = styled.thead`
+    -webkit-box-shadow:-3px 6px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 0px 0 rgb(0 0 0 / 0%);
+    @media (max-width: 767px) {
+        display: block;
+        float: right;
+    }
+`
+export const TableBodyContainer = styled.tbody`
+@media (max-width: 767px) {
+    display: block;
+    width: auto;
+    position: relative;
+    overflow-x: auto;
+}
+`
+export const TableTh = styled.th`
+    text-align: center;
+    padding: 8px;
+    color: #ffffff;
+    background: #7100E1;
+    &:nth-child(odd) {
+    color: #ffffff;
+    background: #5700AD;
+}
+@media (max-width: 767px) {
+    display: block;
+    &:last-child{
+        border-bottom: none;
+    }
+    padding: 20px .625em .625em .625em;
+        height: 60px;
+        vertical-align: middle;
+        box-sizing: border-box;
+        overflow-x: hidden;
+        overflow-y: auto;
+        width: 120px;
+        font-size: 13px;
+        text-overflow: ellipsis;
+        text-align: right;
+        border-bottom: 1px solid #f7f7f9;
+        text-align: right;
+        border-bottom: 1px solid #f7f7f9;
+}
+`
+export const TableTd = styled.td`
+    text-align: right;
+    padding: 8px;
+    border-left: 1px solid #f8f8f8;
+    font-size: 12px;
+    @media (max-width: 767px) {
+        padding: 20px .625em .625em .625em;
+        height: 60px;
+        vertical-align: middle;
+        box-sizing: border-box;
+        overflow-x: hidden;
+        overflow-y: auto;
+        width: 120px;
+        font-size: 13px;
+        text-overflow: ellipsis;
+        &:nth-child(odd) {
+        background: #F8F8F8;
+        border-left: 1px solid #E6E4E4;
+    }
+    &:nth-child(even) {
+        border-left: 1px solid #E6E4E4;
+    }
+        display: block;
+    }
+`
+export const TableTr = styled.tr`
+&:nth-child(even) {
+    background: #F8F8F8;
+}
+@media (max-width: 767px) {
+&:nth-child(even) {
+        background: transparent;
+    }
+    }
+`
