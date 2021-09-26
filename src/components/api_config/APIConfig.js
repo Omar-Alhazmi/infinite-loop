3db1dff32df17b4524ec5c3fe3209448babf5421
Omar Alhazmi <vex.xcc@gmail.com>
2021-09-26 22:46:56 +0300

    add server url

--- src/components/api_config/APIConfig.js
+++ src/components/api_config/APIConfig.js
@@ -2,7 +2,7 @@ let apiURL;
 const expressPort = 5000;
 const apiUrls = {
     development: `http://localhost:${expressPort}/`,
-    production: ``,
+    production: `https://infinite-loop-a.herokuapp.com/`,
 };
 window.location.hostname === 'localhost' ?
     apiURL = apiUrls.development : apiURL = apiUrls.production;
